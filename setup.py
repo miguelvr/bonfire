76e883ca543a171c982af2394f67916f0ec8e158
miguelvr <miguelvramos@ist.utl.pt>
2018-02-15 14:20:34 +0000

    Added setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bonfire',
+    version='0.0.1',
+    packages=['bonfire'],
+    url='',
+    license='',
+    author='miguelvr',
+    author_email='miguelvramos@ist.utl.pt',
+    description='The starting point for your PyTorch models'
+)
